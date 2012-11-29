6ba86d316b63d1898128f641fd7b9d2782c5f268
Biscan <mat.podsiadly@gmail.com>
2012-11-29 15:23:49 +0100

    poczatek klienta z drobnymi komentarzami

--- client.cpp
+++ client.cpp
@@ -1,9 +1,98 @@
 #include "client.h"
-
+#include<iostream>
 Client::Client()
 {
-    //ctor
+    ZeroMemory(&data, sizeof(data)); //funkcja, która zeruje strukturke
+    sock = INVALID_SOCKET;
+    ZeroMemory(&addr, sizeof(addr));
+    host_name = "";
 }
+
+
+
+/** @brief init
+  *
+  * @todo: funkcja inicjalizująca Winsock'a
+  */
+bool Client::init()
+{                                                    //WSAStartup inicjalizuje winsock'a
+    int result = WSAStartup(MAKEWORD(2,2), &data);   //zwraca 0 jesli poprawnie odpalona
+    if(result)                                       //MAKEWORD to makro, które tworzy//drugi parametr to referencja na strukturê WSADATA
+    {
+        std::cout<<"blad inicjalizacji winsock(init)!"<<std::endl;
+        return false;
+    }
+                                                     //funkcja zwracajaca socketa
+    sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);  //pierwszy parametr okresla rodzine adresow z jakiej bedzie korzystal socket; AF_INET to IPv4
+    if(sock == INVALID_SOCKET)                       //drugi parametr to typ socket'a; dla protokolu TCP uzywany jest typ SOCK_STREAM
+    {                                                //trzeci parametr to protokol, z ktorego bedzie korzystal socket; IPPROTO_TCP to prot. TCP
+        std::cout<<"blad socket'a (init)"<<std::endl;//funkcja zwraca INVALID_SOCKET jesli sie nie powiedzie
+        return false;
+    }
+    return true;
+}
+
+/** @brief (one liner)
+  *
+  * funkcja zwracajaca hosta jako liczbe calkowita
+  * pozniej jeszcze dopisze dokladnie o co chodzi:P
+  */
+u_long Client::get_host(const std::string &h_name)
+{
+    HOSTENT *host = gethostbyname(h_name.c_str());
+    if(!host)
+    {
+        u_int address= inet_addr(h_name.c_str());
+        host = gethostbyaddr((char *)address, 4, AF_INET);
+        if(!host)
+        {
+            std::cout<<"nie polaczono z hostem (get_host)!"<<std::endl;
+            return 0;
+        }
+    }
+    return *((u_long*)*host->h_addr_list);
+}
+
+
+
+
+
+
+/** @brief (one liner)
+  *
+  * polaczenie z hostem
+  */
+bool Client::connect_to_host(const std::string &h_name, u_short port)
+{
+    if(init())
+    {
+        host_name = h_name;
+        addr.sin_addr.S_un.S_addr = get_host(h_name);
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        if(connect(sock, (sockaddr*)&addr, sizeof(sockaddr)) == SOCKET_ERROR )
+        {
+            sock = INVALID_SOCKET;
+            std::cout<<"blad gniazda"<<std::endl;
+            std::cout<<"kod bledu = "<<WSAGetLastError()<<std::endl;
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+/** @brief (one liner)
+  *
+  *zamykanie polaczenia
+  */
+void Client::close()
+{
+    closesocket(sock);
+    WSACleanup();
+}
+
+
 
 Client::~Client()
 {
--- client.h
+++ client.h
@@ -1,43 +1,27 @@
 #ifndef CLIENT_H
 #define CLIENT_H
-
-
+#include<winsock2.h>
+#include<string>
 class Client
 {
+    private:
+        WSADATA data; //zmienna, która przechowuje informacje o Winsock'u (wersja itd.)
+        SOCKET sock; //gniazdo dla naszego klienta
+        sockaddr_in addr; //struktura przechowujaca
+        std::string host_name; //nazwa serwera, z którym bedziemy sie laczyc
+
+        bool init();
+        u_long get_host(const std::string &h_name);
+
     public:
         /** Default constructor */
         Client();
         /** Default destructor */
         virtual ~Client();
-        /** Access data
-         * \return The current value of data
-         */
-        WSADATA Getdata() { return data; }
-        /** Set data
-         * \param val New value to set
-         */
-        void Setdata(WSADATA val) { data = val; }
-        /** Access sock
-         * \return The current value of sock
-         */
-        SOCKET Getsock() { return sock; }
-        /** Set sock
-         * \param val New value to set
-         */
-        void Setsock(SOCKET val) { sock = val; }
-        /** Access addr
-         * \return The current value of addr
-         */
-        sockaddr_in Getaddr() { return addr; }
-        /** Set addr
-         * \param val New value to set
-         */
-        void Setaddr(sockaddr_in val) { addr = val; }
-    protected:
-    private:
-        WSADATA data; //!< Member variable "data"
-        SOCKET sock; //!< Member variable "sock"
-        sockaddr_in addr; //!< Member variable "addr"
+
+        bool connect_to_host(const std::string &h_name, u_short port);
+        void close();
+
 };
 
 #endif // CLIENT_H
